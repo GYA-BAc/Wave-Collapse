49a5f9c1b70c705153b2b6d074b5a420c3b771e7
GYA-BAc <97851399+GYA-BAc@users.noreply.github.com>
2022-08-09 17:46:53 -0500

    updated function
    - tiles can affect any tile around them (as opposed to their immediate surroundings as before)
    - tiles can differently affect different faces

--- main.cpp
+++ main.cpp
@@ -6,15 +6,21 @@
 #include <algorithm>
 #include <Windows.h>
 
+//NOTE: rules can only have apply to immediate surroundings (3x3)
 
 struct tile {
     char id;
     std::vector<char> canBe;
 };
 
+struct border_value {
+    std::vector<char> appliesTo;
+    std::vector<int>  position;
+};
+
 struct rule {
     char id;
-    std::vector<char> canBorder;
+    std::vector<border_value> borders;
 };
 
 void stepWaveCollapse(std::vector<std::vector<tile>> &board,
@@ -24,11 +30,6 @@ void stepWaveCollapse(std::vector<std::vector<tile>> &board,
     std::random_device rd;
     std::default_random_engine gen(rd());
     std::uniform_int_distribution<int> distr(0, (board[pos[0]][pos[1]].canBe.size()-1));
-
-    const std::vector<std::vector<int>> surr = {
-        {-1,-1}, {0,-1}, {1,-1}, {-1,0}, {1,0}, {-1,1}, {0,1}, {1,1}
-    };
-    
    
     char nval = board[pos[0]][pos[1]].canBe[distr(gen)];
     board[pos[0]][pos[1]].id = nval;
@@ -41,37 +42,34 @@ void stepWaveCollapse(std::vector<std::vector<tile>> &board,
             break;
         }
     }
-    // std::cout << cur_rule.id << ' ';
-    // for (char c : cur_rule.canBorder) {
-    //     std::cout << c << ' ';
-    // }
-    // std::cout << '\n';
 
-    for (std::vector<int> check : surr) {
+    for (border_value check : cur_rule.borders) {
         //check if the surrounding value is out of bounds
-        if (((pos[0] == 0                 && check[0] == -1) ||
-             (pos[0] == board.size()-1    && check[0] == 1 ) ||
-             (pos[1] == 0                 && check[1] == -1) ||
-             (pos[1] == board[0].size()-1 && check[1] == 1 ))) {
+        if (((pos[0]+check.position[0] < 0                 ) ||
+             (pos[0]+check.position[0] > board.size()-1    ) ||
+             (pos[1]+check.position[1] < 0                 ) ||
+             (pos[1]+check.position[1] > board[0].size()-1 ))) {
             continue;
         }
        
-        
-        tile val = board[pos[0]+check[0]][pos[1]+check[1]];
+        //make a variable to store value at board position being checked (easier to type)
+        tile val = board[pos[0]+check.position[0]][pos[1]+check.position[1]];
 
+        //update tile's list of possible value
         if (val.id != '.') continue;
         for (int i = 0; i < val.canBe.size(); i++) {
-            //remove value from canBe if it is not in canBorder
-            if (cur_rule.canBorder.size() == 1) {
-                val.canBe = cur_rule.canBorder;
+            //remove value from canBe if it is not in list of possible values define by rules
+            if (check.appliesTo.size() == 1) {
+               val.canBe = check.appliesTo;
                 continue;
             }
 
-            if (std::find(cur_rule.canBorder.begin(), cur_rule.canBorder.end(), val.canBe[i]) != cur_rule.canBorder.end()) 
+            if (std::find(check.appliesTo.begin(), check.appliesTo.end(), val.canBe[i]) != check.appliesTo.end()) 
                 continue;
             val.canBe.erase(val.canBe.begin() + i);
         }
-        board[pos[0]+check[0]][pos[1]+check[1]].canBe = val.canBe;
+        //update board value
+        board[pos[0]+check.position[0]][pos[1]+check.position[1]].canBe = val.canBe;
         
     }
 
@@ -81,21 +79,21 @@ void printBoard(std::vector<std::vector<tile>> &board, bool debug) {
     std::string final = "";
     for (std::vector<tile> row : board) {
         for (tile t : row) {
-            switch (t.id) {
-                case '0': {
-                    final += ' ';
-                    break;
-                }
-                case '1': {
-                    final += (char)(219); //█ this character
-                    break;
-                }
-                case '2': {
-                    final += 'O';
-                    break;
-                }
-            }
-            //final += t.id;
+            // switch (t.id) {
+            //     case '0': {
+            //         final += ' ';
+            //         break;
+            //     }
+            //     case '1': {
+            //         final += (char)(219); //█ this character
+            //         break;
+            //     }
+            //     case '2': {
+            //         final += 'O';
+            //         break;
+            //     }
+            // }
+            final += t.id;
             if (!debug) continue;
             final += " (";
             for (char c : t.canBe) {
@@ -114,19 +112,79 @@ void printBoard(std::vector<std::vector<tile>> &board, bool debug) {
 
 int main() {
 
-    rule zero = {'0', std::vector<char>({'0', '1', '2'})};
-    rule one  = {'1', std::vector<char>({'1', '0'})};
-    rule two  = {'2', std::vector<char>({'0'})};
+    //init rules
+    rule zero = {'0', 
+        std::vector<border_value>({
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({-1, -1})}), 
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({-1, 0})}), 
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({-1, 1})}),
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({0, -1})}), 
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({0, 1})}),
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({1, -1})}), 
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({1, 0})}), 
+            border_value({std::vector<char>({'0', '1', '2'}), std::vector<int>({1, 1})}),
+        }),
+    };
+    rule one = {'1',
+        std::vector<border_value>({
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({-1, -1})}), 
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({-1, 0})}), 
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({-1, 1})}),
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({0, -1})}), 
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({0, 1})}),
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({1, -1})}), 
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({1, 0})}), 
+            border_value({std::vector<char>({'1', '0'}), std::vector<int>({1, 1})}),
+        }),
+    };
+    rule two = {'2',
+        std::vector<border_value>({
+            //inner values (3x3)
+            border_value({std::vector<char>({'0'}), std::vector<int>({-1, -1})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({-1, 0})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({-1, 1})}),
+            border_value({std::vector<char>({'0'}), std::vector<int>({0, -1})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({0, 1})}),
+            border_value({std::vector<char>({'0'}), std::vector<int>({1, -1})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({1, 0})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({1, 1})}),
+            
+            //outer values (5x5)
+            border_value({std::vector<char>({'0'}), std::vector<int>({-2, -2})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({-2, -1})}),
+            border_value({std::vector<char>({'0'}), std::vector<int>({-2, 0})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({-2, 1})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({-2, 2})}), 
 
-    tile empty_tile = {'.', std::vector<char>({'0', '1', '2'})};
+            border_value({std::vector<char>({'0'}), std::vector<int>({-1, -2})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({-1, 2})}),
 
+            border_value({std::vector<char>({'0'}), std::vector<int>({0, -2})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({0, 2})}),
+
+            border_value({std::vector<char>({'0'}), std::vector<int>({1, -2})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({1, 2})}),
+
+            border_value({std::vector<char>({'0'}), std::vector<int>({2, -2})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({2, -1})}),
+            border_value({std::vector<char>({'0'}), std::vector<int>({2, 0})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({2, 1})}), 
+            border_value({std::vector<char>({'0'}), std::vector<int>({2, 2})}), 
+        }),
+    };
     const std::vector<rule> ruleset = {zero, one, two};
 
+    //init empty tile
+    std::vector<char> all_options = {};
+    for (rule r : ruleset) {
+        all_options.push_back(r.id);
+    }
+    tile empty_tile = {'.', all_options};
+
     //init board
-    const int nRows = 20, nCols = 50;
+    const int nRows = 10, nCols = 10;
 
     std::vector<std::vector<tile>> board(nRows, std::vector<tile>(nCols, empty_tile));
-    //NOTE : check if occupied tiles have restricted list
 
     //stepWaveCollapse(board, ruleset, std::vector<int>({0,0}), '1');
     for (int i = 0; i < nRows; i++) {
@@ -134,17 +192,13 @@ int main() {
             //system("cls");
             stepWaveCollapse(board, ruleset, std::vector<int>({i,j}));
             //printBoard(board, true);
-            //Sleep(10);
             //std::cin.get();
+            
+            //Sleep(10);
             
         }
     }
     printBoard(board, false);
 
-
-    //std::vector<char> a = {'1', '2', '3'};
-    //if (std::find(a.begin(), a.end(), '1') != a.end()) std::cout << '\a';
-
-   
     return 0;
 }
